3628427c13bf0d878dfcc81455e92e8a25244391
liliane1901 <lili.i01@web.de>
2023-08-02 16:47:27 +0200

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
@@ -8,7 +9,8 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
-  @override
+  @override 
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
